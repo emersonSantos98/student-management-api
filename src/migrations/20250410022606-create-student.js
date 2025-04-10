8a85db14b91dd2a4174f70590bf7f6d6e5a4b7d6
Emerson <emerson.matosokl10@gmail.com>
2025-04-09 23:33:42 -0300

    chore: adicionar modelo Student e migration para a tabela Students

---
+++ src/migrations/20250410022606-create-student.js
@@ -0,0 +1,58 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Students', {
+      id: {
+        type: Sequelize.UUID,
+        primaryKey: true,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+        unique: true,
+      },
+      ra: {
+        type: Sequelize.STRING(20),
+        allowNull: false,
+        unique: true,
+        comment: 'Registro Acadêmico',
+      },
+      cpf: {
+        type: Sequelize.STRING(11),
+        allowNull: false,
+        unique: true,
+        comment: 'CPF sem pontuação',
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now'),
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now'),
+      }
+    });
+    await queryInterface.addIndex('Students', ['ra'], {
+      name: 'idx_students_ra'
+    });
+    await queryInterface.addIndex('Students', ['cpf'], {
+      name: 'idx_students_cpf'
+    });
+    await queryInterface.addIndex('Students', ['email'], {
+      name: 'idx_students_email'
+    });
+    await queryInterface.addIndex('Students', ['created_at'], {
+      name: 'idx_students_created_at'
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Students');
+  }
+};
