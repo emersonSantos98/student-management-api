17e19aa66e7f9c4a6890b976c1d461d41382fb1a
Emerson <emerson.matosokl10@gmail.com>
2025-04-13 23:33:50 -0300

    feat: configurar CORS para permitir requisições do frontend

--- app.js
+++ app.js
@@ -10,8 +10,17 @@ const db = require('./src/models');
 
 const app = express();
 
+const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:5173';
+
+
 // Middlewares 🛡️
 app.use(helmet());
+app.use(cors({
+    origin: FRONTEND_URL,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
