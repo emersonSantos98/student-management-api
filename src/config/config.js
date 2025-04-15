99e0a47e57c53e21fdc34010c9fd2550813f32e5
Emerson <emerson.matosokl10@gmail.com>
2025-04-15 02:17:57 -0300

    feat: simplificar a configuração do dotenv removendo o gerenciamento de arquivos específicos do ambiente.

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,5 @@
 const dotenv = require('dotenv');
 dotenv.config();
-const envFile = process.env.NODE_ENV === 'production' ? '.env.production' : '.env.development';
-dotenv.config({ path: envFile });
 
 module.exports = {
   development: {
